4a7eb28180db805bf98887fb028e63cb87b25db2
Harpal <Harpalsgh@gmail.com>
2019-11-26 16:57:34 -0600

    first commit

---
+++ src/main/java/com/cde/microservices/limitservice/bean/LimitConfiguration.java
@@ -0,0 +1,31 @@
+package com.cde.microservices.limitservice.bean;
+
+public class LimitConfiguration {
+	
+	private int maximum;
+	private int minimun;
+	
+	public LimitConfiguration() {
+		
+	}
+
+	public LimitConfiguration(int maximum, int minimun) {
+		super();
+		this.maximum = maximum;
+		this.minimun = minimun;
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public int getMinimun() {
+		return minimun;
+	}
+	public void setMinimun(int minimun) {
+		this.minimun = minimun;
+	}
+
+}
