17fa9b6c079ef445714160669a2bdb1cc6165d12
Harpal <Harpalsgh@gmail.com>
2019-12-02 16:31:00 -0600

    hystrix

--- src/main/java/com/cde/microservices/limitservice/LimitConfigurationController.java
+++ src/main/java/com/cde/microservices/limitservice/LimitConfigurationController.java
@@ -5,16 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cde.microservices.limitservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
+
 public class LimitConfigurationController {
 	
 	@Autowired
 	private Configuration configuration;
 	
-	@GetMapping("/limits")
+	@GetMapping("/limits")	
 	public LimitConfiguration retrieveLimitConfig() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 
+	@GetMapping("/fault-tolerance") 
+	@HystrixCommand(fallbackMethod = "fallbackConfig")
+	public LimitConfiguration retrieveConfig() {
+		throw new RuntimeException("Not Avaialable");
+	}
+	
+	public LimitConfiguration fallbackConfig() {
+		return new LimitConfiguration(9, 9999);
+	}
 }
